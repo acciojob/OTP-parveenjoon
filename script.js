2350bf67b88e56a6420e74203d10602325adf744
parveenjoon <47267545+parveenjoon@users.noreply.github.com>
2024-05-31 11:06:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+
 //your JS code here. If required.
+const inputs = document.querySelectorAll(".code");
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  inputs[0].focus();
+});
+
+inputs.forEach((input, index) => {
+  input.addEventListener("input", (event) => {
+    /^\d+$/.test(event.target.value) &&
+      index < inputs.length - 1 &&
+      inputs[index + 1].focus();
+  });
+
+  input.addEventListener("keydown", (event) => {
+    event.key === "Backspace" &&
+      event.target.value == "" &&
+      index > 0 &&
+      inputs[index - 1].focus();
+  });
+});
